84aa2cd68b159b5ed4654bca582e870948d99fd2
phllp <beigerfelipe@gmail.com>
2024-12-11 10:35:14 -0300

    feat: add trycatch on DirectionsService

--- nestjs-api/src/maps/directions/directions.service.ts
+++ nestjs-api/src/maps/directions/directions.service.ts
@@ -21,29 +21,34 @@ export class DirectionsService {
       key: this.configService.get('GOOGLE_MAPS_API_KEY'),
     };
 
-    const { data } = await this.googleMapsClient.directions({
-      params: requestParams,
-    });
+    try {
+      const { data } = await this.googleMapsClient.directions({
+        params: requestParams,
+      });
 
-    return {
-      ...data,
-      request: {
-        origin: {
-          placeId: requestParams.origin,
-          location: {
-            lat: data.routes[0].legs[0].start_location.lat,
-            lng: data.routes[0].legs[0].start_location.lng,
+      return {
+        ...data,
+        request: {
+          origin: {
+            placeId: requestParams.origin,
+            location: {
+              lat: data.routes[0].legs[0].start_location.lat,
+              lng: data.routes[0].legs[0].start_location.lng,
+            },
           },
-        },
-        destination: {
-          placeId: requestParams.destination,
-          location: {
-            lat: data.routes[0].legs[0].end_location.lat,
-            lng: data.routes[0].legs[0].end_location.lng,
+          destination: {
+            placeId: requestParams.destination,
+            location: {
+              lat: data.routes[0].legs[0].end_location.lat,
+              lng: data.routes[0].legs[0].end_location.lng,
+            },
           },
+          mode: requestParams.mode,
         },
-        mode: requestParams.mode,
-      },
-    };
+      };
+    } catch (error) {
+      console.error('Error directions', error.data);
+      throw error;
+    }
   }
 }
