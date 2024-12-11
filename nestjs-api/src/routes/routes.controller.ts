950e4165817ad063c33ab5cc088584fcea0a20e7
phllp <beigerfelipe@gmail.com>
2024-12-11 10:36:46 -0300

    feat: major updates os RoutesModule

--- nestjs-api/src/routes/routes.controller.ts
+++ nestjs-api/src/routes/routes.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
-import { UpdateRouteDto } from './dto/update-route.dto';
 
 @Controller('routes')
 export class RoutesController {
@@ -19,16 +18,6 @@ export class RoutesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.routesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
-    return this.routesService.update(+id, updateRouteDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.routesService.remove(+id);
+    return this.routesService.findOne(id);
   }
 }
--- nestjs-api/src/routes/routes.module.ts
+++ nestjs-api/src/routes/routes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { MapsModule } from 'src/maps/maps.module';
+import { RoutesDriverService } from './routes-driver/routes-driver.service';
 
 @Module({
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, RoutesDriverService],
+  imports: [MapsModule],
 })
 export class RoutesModule {}
