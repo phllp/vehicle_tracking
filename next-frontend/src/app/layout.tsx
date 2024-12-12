8a3b8878f4ae1bbda37b68bc93e97079871d09e1
phllp <beigerfelipe@gmail.com>
2024-12-12 14:39:48 -0300

    fix: add h-screen to root layout

--- next-frontend/src/app/layout.tsx
+++ next-frontend/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-default`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-default h-screen`}
       >
         {children}
       </body>
